1b4d49956398422c9995aefada3f96223996913c
coconutDreams-56 <cabreraheily@gmail.com>
2025-07-11 02:06:40 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ Supply a member function reset_sales that resets any counters and totals so that
 #include <iostream>
 #include <vector>
 
-using namespace std
+using namespace std;
 
 class CashRegister
 {
@@ -35,12 +35,12 @@ CashRegister()
 
 void clear() 
 {
-if (items.empty())
+if (!items.empty())
 {
-  sales_tot += get_tot;
+  sales_tot += get_tot();
   sales_count++;
 }
-items.clear;
+items.clear();
 }
 
 void add_item(double price) 
@@ -48,13 +48,13 @@ void add_item(double price)
         items.push_back(price);
     }
 
-double get_total() const 
+double get_tot() const 
 {
-        double total = 0;
+        double tot = 0;
         for (double price : items) {
-            total += price;
+            tot += price;
         }
-        return total;
+        return tot;
 }
 
 int get_count() const 
@@ -73,7 +73,7 @@ void display_all() const
 
 double get_sales_total() const 
     {
-        return sales_total;
+        return sales_tot;
     }
 
 int get_sales_count() const 
@@ -84,7 +84,7 @@ int get_sales_count() const
 
 void reset_sales() 
     {
-        sales_total = 0;
+        sales_tot = 0;
         sales_count = 0;
     }
 };
@@ -92,29 +92,29 @@ void reset_sales()
 
 int main()
   {
-  CashRegister register;
+  CashRegister myregister;
 
-    register1.add_item(3.29);
-    register1.add_item(7.79);
-    register1.add_item(1.25);
-    register1.display_all();
-    cout << "Total: $" << register.get_total() << endl;
+    myregister.add_item(3.29);
+    myregister.add_item(7.79);
+    myregister.add_item(1.25);
+    myregister.display_all();
+    cout << "Total: $" << register.get_tot() << endl;
     cout << "Item count: " << register.get_count() << endl;
-    register.clear(); // Ends transaction
+    myregister.clear(); // Ends transaction
 
-    register1.add_item(5.99);
-    register1.add_item(2.50);
+    myregister.add_item(5.99);
+    myregister.add_item(2.50);
     cout << "\nSecond customer:\n";
-    register1.display_all();
-    cout << "Total: $" << register.get_total() << endl;
-    register1.clear(); // Ends transaction
+    myregister.display_all();
+    cout << "Total: $" << register.get_tot() << endl;
+    myregister.clear(); // Ends transaction
 
     cout << "\nEnd of day report:" << endl;
     cout << "Total sales: $" << register.get_sales_total() << endl;
     cout << "Number of transactions: " << register.get_sales_count() << endl;
 
     
-    register.reset_sales();
+    myregister.reset_sales();
     cout << "\nSales reset for next day." << endl;
     cout << "Total sales: $" << register.get_sales_total() << endl;
     cout << "Number of transactions: " << register.get_sales_count() << endl;
